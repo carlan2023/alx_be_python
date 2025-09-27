62a6edeb654bdf07e84bfd280381bf8a46b1b415
Allan Mitanda <alan@globalwesttechnologies.org>
2025-09-27 13:31:31 +0300

    FNS and DSA

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation (num1, num2, operation):
+    
+    if operation.lower() == 'add':
+        result = num1 + num2
+        return result
+    elif operation == 'subtract':
+        result = num1 - num2
+        return result
+    elif operation == 'multiply':
+        result = num1 * num2
+        return result
+    elif operation == 'divide':
+        if num2 != 0:
+            result = num1 / num2
+            return result
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operation"
