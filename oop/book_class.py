8cdaad82d394c9d5c720040b8b2f37b447138e4b
Allan Mitanda <alan@globalwesttechnologies.org>
2025-10-19 08:53:06 +0300

    OOP

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,17 +1,18 @@
 class Book:
     def __init__(self, title, author, year):
+        """Constructor that initializes the Book instance."""
         self.title = title
         self.author = author
         self.year = year
-    
-    def __del__(self):
-        print(f"Deleting {self.title}")
-        
+
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.year_published})"
-    
+        """User-friendly string representation."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
     def __repr__(self):
+        """Official string representation that can recreate the object."""
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    # def get_summary(self):
-    #     return f"'{self.title}' by {self.author}, published in {self.year_published}."
+    def __del__(self):
+        """Destructor called when the object is deleted."""
+        print(f"Deleting {self.title}")
